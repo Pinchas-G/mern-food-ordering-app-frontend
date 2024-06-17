62aa0be42d65da78edb85cdfaa62c0744a2e00d4
Pinchas <123pinchas@gmail.com>
2024-06-17 18:42:37 +0300

    adding view order features

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -1,8 +1,39 @@
+import { Order } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch orders");
+    }
+
+    return response.json();
+  };
+
+  const { data: orders, isLoading } = useQuery(
+    "fetch my orders",
+    getMyOrdersRequest,
+    {
+      refetchInterval: 5000,
+    }
+  );
+
+  return { orders, isLoading };
+};
 
 type CheckoutSessionRequest = {
   cartItems: {
@@ -26,14 +57,17 @@ export const useCreateCheckoutSession = () => {
     checkoutSessionRequest: CheckoutSessionRequest
   ) => {
     const accessToken = await getAccessTokenSilently();
-    const response = await fetch(`${API_BASE_URL}/api/order/checkout/create-checkout-session`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(checkoutSessionRequest),
-    });
+    const response = await fetch(
+      `${API_BASE_URL}/api/order/checkout/create-checkout-session`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkoutSessionRequest),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Unable to create checkout session");
